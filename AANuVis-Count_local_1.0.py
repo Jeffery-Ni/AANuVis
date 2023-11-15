15ef80ee1c53dd1ba1813efa358bd840468bcf38
Y. N <112688058+Jeffery-Ni@users.noreply.github.com>
2023-11-15 20:58:12 +0800

    Update AANuVis-Count_local_1.01.py

--- AANuVis-Count_local_1.0.py
+++ AANuVis-Count_local_1.0.py
@@ -5,6 +5,7 @@
 #DiNucl count order: AA,AC,AG,AT,CA,CC,CG,CT,GA,GC,GG,GT,TA,TC,TG,TT
 #Tetra count order: AAAA,AAAC,AAAG,AAAT,AACA,AACC,AACG,AACT,AAGA,AAGC,AAGG,AAGT,AATA,AATC,AATG,AATT,ACAA,ACAC,ACAG,ACAT,ACCA,ACCC,ACCG,ACCT,ACGA,ACGC,ACGG,ACGT,ACTA,ACTC,ACTG,ACTT,AGAA,AGAC,AGAG,AGAT,AGCA,AGCC,AGCG,AGCT,AGGA,AGGC,AGGG,AGGT,AGTA,AGTC,AGTG,AGTT,ATAA,ATAC,ATAG,ATAT,ATCA,ATCC,ATCG,ATCT,ATGA,ATGC,ATGG,ATGT,ATTA,ATTC,ATTG,ATTT,CAAA,CAAC,CAAG,CAAT,CACA,CACC,CACG,CACT,CAGA,CAGC,CAGG,CAGT,CATA,CATC,CATG,CATT,CCAA,CCAC,CCAG,CCAT,CCCA,CCCC,CCCG,CCCT,CCGA,CCGC,CCGG,CCGT,CCTA,CCTC,CCTG,CCTT,CGAA,CGAC,CGAG,CGAT,CGCA,CGCC,CGCG,CGCT,CGGA,CGGC,CGGG,CGGT,CGTA,CGTC,CGTG,CGTT,CTAA,CTAC,CTAG,CTAT,CTCA,CTCC,CTCG,CTCT,CTGA,CTGC,CTGG,CTGT,CTTA,CTTC,CTTG,CTTT,GAAA,GAAC,GAAG,GAAT,GACA,GACC,GACG,GACT,GAGA,GAGC,GAGG,GAGT,GATA,GATC,GATG,GATT,GCAA,GCAC,GCAG,GCAT,GCCA,GCCC,GCCG,GCCT,GCGA,GCGC,GCGG,GCGT,GCTA,GCTC,GCTG,GCTT,GGAA,GGAC,GGAG,GGAT,GGCA,GGCC,GGCG,GGCT,GGGA,GGGC,GGGG,GGGT,GGTA,GGTC,GGTG,GGTT,GTAA,GTAC,GTAG,GTAT,GTCA,GTCC,GTCG,GTCT,GTGA,GTGC,GTGG,GTGT,GTTA,GTTC,GTTG,GTTT,TAAA,TAAC,TAAG,TAAT,TACA,TACC,TACG,TACT,TAGA,TAGC,TAGG,TAGT,TATA,TATC,TATG,TATT,TCAA,TCAC,TCAG,TCAT,TCCA,TCCC,TCCG,TCCT,TCGA,TCGC,TCGG,TCGT,TCTA,TCTC,TCTG,TCTT,TGAA,TGAC,TGAG,TGAT,TGCA,TGCC,TGCG,TGCT,TGGA,TGGC,TGGG,TGGT,TGTA,TGTC,TGTG,TGTT,TTAA,TTAC,TTAG,TTAT,TTCA,TTCC,TTCG,TTCT,TTGA,TTGC,TTGG,TTGT,TTTA,TTTC,TTTG,TTTT
 #Usage: AANuVis-count_local.py import.fasta , no none-verbose option is provided, so recommended nohup (-) & for huge files 
+#1.01 version fixed the issue if there are special characters in seq names
 
 import os
 import re
@@ -16,12 +17,13 @@ def run_prodigal(seq_id):
     with open(f'{seq_id}_modified.fasta', 'w') as file:
         file.write(f'>{seq_id}\n')
         file.write(sequence)
-    os.system(f'prodigal -i {seq_id}_modified.fasta -a {seq_id}.faa -t {fasta_file}_train')
-    os.system(f'rm {seq_id}_modified.fasta')
+    os.system(f"prodigal -i '{seq_id}_modified.fasta' -a '{seq_id}.faa' -t '{fasta_file}_train'")
+    os.system(f"rm '{seq_id}_modified.fasta'")
     
 def delete_files(seq_id):
-    os.system(f'rm {seq_id}.fasta')
-    os.system(f'rm {seq_id}.faa')
+    os.system(f"rm '{seq_id}.fasta'")
+    os.system(f"rm '{seq_id}.faa'")
+
 def calculate_dinucleotide_count(seq_id):
     nucleotides = ['A', 'C', 'G', 'T']
     dinucleotides = [a+b for a in nucleotides for b in nucleotides]
